daeb34e63d8688cbe1645b599b6be64a6864ca92
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-04-10 22:37:23 +0530

    Create  Round Off.java

---
+++ Input & Output/Round Off.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float num=sc.nextFloat();
+        System.out.println((int)Math.floor(num));
+        System.out.println((int)Math.ceil(num));
+        System.out.println((int)Math.floor(num));
+
+    }
+}
