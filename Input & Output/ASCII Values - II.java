1d99e469b3bdbcef62fd1fe7ab3c627a16409ecc
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-04-10 22:34:38 +0530

    Create ASCII Values - II.java

---
+++ Input & Output/ASCII Values - II.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println((char)num);
+    }
+}
