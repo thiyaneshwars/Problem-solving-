9a42c1321a165f695053de54bd9a9c79752f7a5c
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-04-11 21:22:36 +0530

    Create Multiplication Table.java

---
+++ Control Statements /Multiplication Table.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        System.out.println("Enter m");
+        int m = sc.nextInt();
+        System.out.println("The multiplication table of "+n+" is");
+
+        for(int i=1;i<=m;i++){
+            int num = i*n;
+            System.out.println(i+"*"+n+"="+num);
+        }
+    }
+}
