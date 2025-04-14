ccee638a0d43c837e28ef76bea90e2b36c927367
Thiyaneshwar S <138683953+thiyaneshwars@users.noreply.github.com>
2025-04-14 20:14:22 +0530

    Create Transpose Matrix.java

---
+++ Arrays 2D/Transpose Matrix.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+         int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+            
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Transpose matrix is:");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
